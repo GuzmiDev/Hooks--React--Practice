f69e79f9bd87b130ef3c66a9bef5aafe58efa8a7
Erick Guzman <guzmi.dev@gmail.com>
2022-03-28 22:55:44 -0700

    :art: customHook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById("root"));
