1a8a44c1b73c216a2e1edf093129e85a66db34b4
Erick Guzman <guzmi.dev@gmail.com>
2022-03-29 22:35:31 -0700

    :art: Idea general de un reducer

---
+++ src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,33 @@
+const initialState = [
+  {
+    id: 5,
+    todo: "Hacer ejercicio",
+    done: false,
+  },
+];
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "agregar":
+      return (state = [...state, action.payload]);
+    default:
+      return state;
+  }
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 6,
+  todo: "Aprender Angular",
+  done: true,
+};
+
+const action = {
+  type: "agregar",
+  payload: newTodo,
+};
+
+todos = todoReducer(initialState, action);
+
+console.log(todos);
